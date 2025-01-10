047a467c7acdab9aa0ac082c7747e1022d902606
Vishnu Kumar <161946782+Vishnukrchy@users.noreply.github.com>
2025-01-10 16:53:46 +0530

    crude

--- todo/main.py
+++ todo/main.py
@@ -7,22 +7,61 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/get-todos")  
-def get_todos():
-    print("get-todos is called")
-    return "get-todos is called"
+# Read data from frontend and return it back
+# 1) by path variable    
+# get by id by passing id as parameter as part of the url or path variable
+@app.get("/get-todo/{id}")
+def get_todos(id: int):
+    print("get-todo is called")
+    return "get-todo is called, id: " + str(id)
+@app.delete("/delete-todo/{id}")
+def delete_todo(id: int):
+    print("delete-todo is called")
+    return "delete-todo is called, id: " + str(id)
+@app.get("/get-todo")
+def get_todos(id: int, name: str):
+   print("get-todo is called", id, name)
+   return {"id": id, "name": name}
 
-@app.post("/add-todo")
-def add_todo():
-    print("add-todo is called")
-    return "add-todo is called"
+student = [{
+    "id": 1,
+    "name": "John"
+}, {
+    "id": 2,
+    "name": "Doe"
+}]
+@app.get("/get-student")
+def get_student():
+    print("get-student is called")
+    return student
+@app.post("/add-student")
+def add_student(id: int, name: str):
+    print("add-student is called")
+    global student
+    student.append({"id": id, "name": name})
+    return student  
+@app.put("/update-student")
+def update_student(id: int, name: str):
+  global student
+  for s in student:
+    if s["id"] == id:
+        s["name"]=name
+        return student
 
-@app.put("/update-todo")
-def update_todo():
-    print("update-todo is called")
-    return "update-todo is called"  
-    
-
+@app.delete("/delete-student/{id}")
+def delete_student(id: int):
+    global student
+    for s in student:
+      if s["id"] == id:
+        student.remove(s)
+        return student
+@app.get("/get-student/{id}")
+def get_student_by_id(id: int):
+    global student
+    for s in student:
+       if s["id"]==id:
+          return s    
+#3) by request body => get by id by passing id as part of the request body => postman => body => raw => json
 
 
 """
